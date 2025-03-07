6794c4ced54d7e411a8360b193f5590de9566146
KwonSumin <rbalstnals1920@gmail.com>
2025-03-07 11:49:22 +0900

    Add Asset

---
+++ Assets/Script/Player/PlayerCondition.cs
@@ -0,0 +1,34 @@
+using System;
+using UnityEngine;
+
+// UI를 참조할 수 있는 PlayerCondition
+// 외부에서 능력치 변경 기능은 이곳을 통해서 호출. 내부적으로 UI 업데이트 수행.
+public class PlayerCondition : MonoBehaviour
+{
+    public UICondition uiCondition;
+
+    Condition health { get { return uiCondition.health; } }
+    Condition stamina { get { return uiCondition.stamina; } }
+  // hunger가 0일때 사용할 값 (value > 0)
+    public event Action onTakeDamage;   // Damage 받을 때 호출할 Action (6강 데미지 효과 때 사용)
+
+    private void Update()
+    {
+        stamina.Add(stamina.passiveValue * Time.deltaTime);
+
+        if (health.curValue < 0f)
+        {
+            Die();
+        }
+    }
+
+    public void Heal(float amount)
+    {
+        health.Add(amount);
+    }
+
+    public void Die()
+    {
+        Debug.Log("플레이어가 죽었다.");
+    }
+}
